b0c615fab4d0bee64fe3241574664bc3fc009ed3
Martin <fejtimartin98@gmail.com>
2020-03-10 22:00:23 +0100

    - Add unit test to bolygok project

--- 01/01/bolygok/src/main/java/hu/me/krz/haladojava/Point.java
+++ 01/01/bolygok/src/main/java/hu/me/krz/haladojava/Point.java
@@ -31,4 +31,32 @@ public final class Point {
         
         return new Point(newX, newY, newZ);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (x ^ (x >>> 32));
+        result = prime * result + (int) (y ^ (y >>> 32));
+        result = prime * result + (int) (z ^ (z >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Point other = (Point) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        if (z != other.z)
+            return false;
+        return true;
+    }
 }
--- 01/01/bolygok/src/test/java/hu/me/krz/haladojava/AppTest.java
+++ 01/01/bolygok/src/test/java/hu/me/krz/haladojava/AppTest.java
@@ -2,6 +2,13 @@ package hu.me.krz.haladojava;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -12,9 +19,45 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    @Ignore
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    
+    @Test
+    public void getName() {
+        String name = "Nem Tom";
+        
+        Astronomer astronomer = new Astronomer(name);
+        
+        //assertTrue(astronomer.getName().equals(name));
+        assertThat(astronomer.getName(), is(equalTo(name)));
+    }
+    
+    @Ignore
+    @Test
+    public void getDiscoveredPlanets() {
+        Astronomer astronomer = new Astronomer("Zsemleplanet");
+        Point point = new Point(10, 10, 10);
+        Planet planet = new Planet(point, 50, "Zsemleplanet");
+        ArrayList<Planet> planets = new ArrayList<>();
+        planets.add(planet);
+        
+        assertThat(astronomer.getDiscoveredPlanets(), is(equalTo(planets)));
+    }
+    
+    @Test
+    public void observeTheSky() {
+//        String name = "Nem Tom";
+//        Astronomer astronomer = new Astronomer(name);
+//        
+//        astronomer.observeTheSky();
+//        
+//        assertThat(astronomer.getDiscoveredPlanets().size(), is(equalTo(10)));
+//        assertThat(astronomer.getDiscoveredPlanets().get(0).getPosition()), is(equalTo(new Point(0, 0, 0)));
+//        assertThat(astronomer.getDiscoveredPlanets(),get(0).getName(), is(equalTo(String.format("%s 1", name))));
+        
     }
 }
