a931ae5dff49dd2f8b5792be655e66c7e20ac38c
Martin <fejtimartin98@gmail.com>
2020-03-11 13:16:11 +0100

    - Implement komplex feladat as a maven project

---
+++ komplex/src/test/java/komplex/komplex/UserControllerTest.java
@@ -0,0 +1,38 @@
+package komplex.komplex;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserControllerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public UserControllerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( UserControllerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
