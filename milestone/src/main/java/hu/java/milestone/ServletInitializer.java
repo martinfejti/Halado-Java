78dc064059c9966ade7e1e6954efc5b6aea82972
Martin <fejtimartin98@gmail.com>
2020-05-11 17:58:25 +0200

    - Create milestone project

---
+++ milestone/src/main/java/hu/java/milestone/ServletInitializer.java
@@ -0,0 +1,13 @@
+package hu.java.milestone;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MilestoneApplication.class);
+	}
+
+}
