bc9d48a39ae6f6fb11eac58fe28f37e1cbf13c0e
Martin <fejtimartin98@gmail.com>
2020-05-11 18:21:09 +0200

    - Create model classes

---
+++ milestone/src/main/java/hu/java/milestone/model/Document.java
@@ -0,0 +1,44 @@
+package hu.java.milestone.model;
+
+import java.io.File;
+
+public class Document {
+
+    private File file;
+    private boolean approvedByUser;
+    private boolean approvedByAdmin;
+    
+    public Document(File file) {
+        this.file = file;
+        this.approvedByUser = true;
+        this.approvedByAdmin = false;
+    }
+    
+    public Document() {
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public boolean isApprovedByUser() {
+        return approvedByUser;
+    }
+
+    public void setApprovedByUser(boolean approvedByUser) {
+        this.approvedByUser = approvedByUser;
+    }
+
+    public boolean isApprovedByAdmin() {
+        return approvedByAdmin;
+    }
+
+    public void setApprovedByAdmin(boolean approvedByAdmin) {
+        this.approvedByAdmin = approvedByAdmin;
+    }
+    
+}
